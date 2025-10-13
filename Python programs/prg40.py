1ce0023c28095959cae17198db165a8f7479c7e1
Srijit Roy <rsrijit436@gmail.com>
2025-10-13 23:00:32 +0530

    modifications

--- Python programs/prg40.py
+++ Python programs/prg40.py
@@ -6,7 +6,7 @@ I am very excited about it"""
 with open('source.txt','w') as file:
     file.write(sent)
 patt=re.compile(r'^This')
-with open('source.txt','r') as infile, open('dest.txt','w') as outfile:
+with open('source.txt','r') as infile, open('desti.txt','w') as outfile:
     for line in infile:
         if patt.match(line):
             modify=re.sub(r'^This','That',line)
