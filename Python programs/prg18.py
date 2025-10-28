0dbe7cfc930b411aee8738a5d73195ea74eecbc8
Srijit Roy <rsrijit436@gmail.com>
2025-10-29 03:13:32 +0530

    Changes made

--- Python programs/prg18.py
+++ Python programs/prg18.py
@@ -1,12 +1,12 @@
+import os
 cl=0
 cc=0
-with open("sample.txt",'r') as f1:
+file_path = os.path.join(os.path.dirname(__file__), "sample.txt")
+with open(file_path,'r') as f1:
     for line in f1.readlines():
         cl+=1
-f1.close()
-with open("sample.txt",'r') as f2:
+with open(file_path,'r') as f2:
     for line in f2.read():
         cc+=1
-f2.close()
 print("No of lines:",cl)
 print("No of characters:",cc)
